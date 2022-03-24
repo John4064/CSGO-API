fdb773f4f5ef9fb78a2f2558db645c0def29c3cc
John Parkhurst <jparkhurst120@gmail.com>
2022-03-24 12:47:03 -0400

    Int

---
+++ src/main/java/tech/parkhurst/restapi/RestapiApplication.java
@@ -0,0 +1,13 @@
+package tech.parkhurst.restapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestapiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestapiApplication.class, args);
+    }
+
+}
