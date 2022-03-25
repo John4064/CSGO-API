5a6adf22c8e6adec0fadf30ca6bf59c257e03ebe
John Parkhurst <jparkhurst120@gmail.com>
2022-03-24 21:08:43 -0400

    Controller

--- src/main/java/tech/parkhurst/restapi/MatchController.java
+++ src/main/java/tech/parkhurst/restapi/MatchController.java
@@ -1,16 +1,27 @@
 package tech.parkhurst.restapi;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api")
 public class MatchController {
 
-    @GetMapping("/")
-    public String test(){
-        return "abc";
+    @Autowired
+    private MatchServices services;
+
+    @GetMapping("/findall")
+    public ResponseEntity<List<HltvMatch>> test(){
+        return ResponseEntity.ok(services.getUserList());
+    }
+    @GetMapping("/error")
+    public String albino(){
+        return "Error";
     }
 }
