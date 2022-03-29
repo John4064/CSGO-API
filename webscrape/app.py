2b8afc15a87a05ddd4f95d265f9d02230e1f0fb1
John Parkhurst <jparkhurst120@gmail.com>
2022-03-29 01:29:41 -0400

    base

---
+++ webscrape/app.py
@@ -0,0 +1,9 @@
+#Author: John Parkhurst
+#Brief: Selenium Web Scraping application for hltv.org
+
+class HltvScraper():
+
+
+    def __init__(self):
+        self.x=1
+        print(1)
---
+++ webscrape/config.py
@@ -0,0 +1,1 @@
+url = "https://www.hltv.org/results"
