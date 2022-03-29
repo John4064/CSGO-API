4afb11b4180d27696dc44d56cde74e6077baafee
John Parkhurst <jparkhurst120@gmail.com>
2022-03-29 11:31:39 -0400

    Find Elements

--- webscrape/app.py
+++ webscrape/app.py
@@ -1,5 +1,7 @@
 # Author: John Parkhurst
 # Brief: Selenium Web Scraping application for hltv.org
+from selenium.webdriver.common.by import By
+
 from config import *
 from selenium import webdriver
 import re
@@ -7,21 +9,26 @@ import re
 
 class HltvScraper():
 
+
+    #Looking for class result-con
     def gatherData(self) -> None:
         """
         @brief: Gathers the html content that we want from the page
         :return: None
         """
-        print(1)
+        resultData = login_form = self.driver.find_elements(by=By.CLASS_NAME,value="result-con")
+        print(resultData)
         return
 
     def __init__(self):
         print("Scrape Initiated")
         self.driver = webdriver.Firefox()
         self.driver.get(url)
-
-        self.gatherData()
-
-
+        try:
+            self.gatherData()
+        except:
+            self.driver.close()
+            print("ERROR")
+            return
         self.driver.close()
         print("DONE")
