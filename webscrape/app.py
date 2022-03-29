cbbd345ccdda7c9507a771986cd83a2cf35a2441
John Parkhurst <jparkhurst120@gmail.com>
2022-03-29 11:02:41 -0400

    scrape

--- webscrape/app.py
+++ webscrape/app.py
@@ -1,14 +1,27 @@
-#Author: John Parkhurst
-#Brief: Selenium Web Scraping application for hltv.org
+# Author: John Parkhurst
+# Brief: Selenium Web Scraping application for hltv.org
 from config import *
 from selenium import webdriver
 import re
 
+
 class HltvScraper():
 
+    def gatherData(self) -> None:
+        """
+        @brief: Gathers the html content that we want from the page
+        :return: None
+        """
+        print(1)
+        return
 
     def __init__(self):
         print("Scrape Initiated")
         self.driver = webdriver.Firefox()
         self.driver.get(url)
+
+        self.gatherData()
+
+
+        self.driver.close()
         print("DONE")
