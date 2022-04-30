877ef002123172bff5a632129e1c9146c51e249a
John Parkhurst <jparkhurst120@gmail.com>
2022-04-30 16:18:13 -0400

    readability
    just removing dead code+spacing

--- src/main/java/tech/parkhurst/restapi/entities/HltvMatch.java
+++ src/main/java/tech/parkhurst/restapi/entities/HltvMatch.java
@@ -8,24 +8,29 @@ public class HltvMatch {
 
     @Column(name="team_A")
     private String teamA;
+
     @Column(name="team_B")
     private String teamB;
+
     @Column(name="match_url")
     private String url;
+
     @Column(name="competition")
     private String competition;
+
     @Column(name="type_of_match")
     private String typeofmatch;
+
     @Id
     @Column(name="match_id")
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int matchid;
-    //    @Column(name="team_B")
-    //    private String teamB;
+
     @Column(name="score_tA")
     private int scoreA;
+
     @Column(name="score_tB")
     private int scoreB;
+
     //Accessors
     public int getScoreA() {
         return scoreA;
