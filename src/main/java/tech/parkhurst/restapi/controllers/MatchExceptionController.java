4973a10f5d6ba2707c8cbae37c3fa12ce3b88e08
John Parkhurst <jparkhurst120@gmail.com>
2022-04-30 18:30:31 -0400

    Finished up handling

--- src/main/java/tech/parkhurst/restapi/controllers/MatchExceptionController.java
+++ src/main/java/tech/parkhurst/restapi/controllers/MatchExceptionController.java
@@ -4,6 +4,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import tech.parkhurst.restapi.exceptions.MatchNotfoundException;
+import tech.parkhurst.restapi.exceptions.MatchIdFoundException;
 
 @ControllerAdvice
 public class MatchExceptionController {
@@ -11,6 +12,11 @@ public class MatchExceptionController {
     @ExceptionHandler(value = MatchNotfoundException.class)
     public ResponseEntity<Object> exception(MatchNotfoundException exception) {
         return ResponseEntity.ok().body("No match has been found!");
+    }
+
+    @ExceptionHandler(value = MatchIdFoundException.class)
+    public ResponseEntity<Object> exception(MatchIdFoundException exception) {
+        return ResponseEntity.ok().body("This Match already exists in the db!");
     }
 
 }
