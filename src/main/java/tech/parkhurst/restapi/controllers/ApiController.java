92d2424a5b80d8f57118d0d3d5830280967b0462
John Parkhurst <jparkhurst120@gmail.com>
2022-09-29 14:20:19 -0400

    Sonar

--- src/main/java/tech/parkhurst/restapi/controllers/ApiController.java
+++ src/main/java/tech/parkhurst/restapi/controllers/ApiController.java
@@ -1,7 +1,6 @@
 package tech.parkhurst.restapi.controllers;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class ApiController {
 
     @RequestMapping("/*")
-    public ResponseEntity catchAll(){
+    public ResponseEntity<String> catchAll(){
         //Catch any unspecified endpoints
         return ResponseEntity.ok().body("This End Point does not Exist!");
     }
