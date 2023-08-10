3631bb3ed2a67731231944b5fca0875fc8a0be91
John Parkhurst <jparkhurst120@gmail.com>
2023-08-10 17:27:56 -0400

    Migration Complete
    Upgraded Spring from 2.6.5 to 3.1.2. AS well as swapped around a bunch of dependencies as needed

--- src/main/java/tech/parkhurst/restapi/RestapiApplication.java
+++ src/main/java/tech/parkhurst/restapi/RestapiApplication.java
@@ -2,7 +2,14 @@ package tech.parkhurst.restapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.stereotype.Component;
 
+@EnableJpaRepositories("tech.parkhurst.restapi.*")
+@EntityScan("tech.parkhurst.restapi.*")
+@ComponentScan(basePackages = { "tech.parkhurst.restapi.*" })
 @SpringBootApplication
 public class RestapiApplication {
 
