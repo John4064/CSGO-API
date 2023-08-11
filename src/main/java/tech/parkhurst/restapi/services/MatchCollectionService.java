dbcf44d959030c9ef17b83fe3a67b8c9ef5fa4c4
John Parkhurst <jparkhurst120@gmail.com>
2023-08-11 01:58:44 -0400

    Initial Connection Made
                        .userAgent("Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:109.0) Gecko/20100101 Firefox/115.0")
     Super important to make sure this is updated or fails

--- src/main/java/tech/parkhurst/restapi/services/MatchCollectionService.java
+++ src/main/java/tech/parkhurst/restapi/services/MatchCollectionService.java
@@ -1,15 +1,42 @@
 package tech.parkhurst.restapi.services;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import static java.rmi.server.LogStream.log;
 
 @Component
 public class MatchCollectionService {
+
+
     @PostConstruct
-    public void init(){
+    public void init() throws IOException {
         System.out.println("GATHER STATISTICS");
+
+        try{
+            Document doc = Jsoup.connect("https://hltv.org/")
+                    .header("Content-Type","application/x-www-form-urlencoded")
+                    .header("Referrer Policy","strict-origin-when-cross-origin")
+                    .userAgent("Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:109.0) Gecko/20100101 Firefox/115.0")
+                    .referrer("http://www.google.com")
+                    .get();
+            System.out.println(doc.title());
+            //Elements newsHeadlines = doc.select("#mp-itn b a");
+        }catch (Exception e){
+            System.out.println("Shit Failed");
+            System.exit(32);
+        }
     }
 
 
