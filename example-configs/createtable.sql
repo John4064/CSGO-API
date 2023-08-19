f3c0456dca48397945d55200b07ade30fdccad26
John Parkhurst <jparkhurst120@gmail.com>
2023-08-19 16:53:04 -0400

    Update createtable.sql

--- example-configs/createtable.sql
+++ example-configs/createtable.sql
@@ -4,7 +4,7 @@ create table hltv_match(
     match_url varchar(255),
     competition varchar(255),
     type_of_match varchar(255),
-    score_ta varchar(255),
-    score_tb varchar(255),
+    score_ta Integer,
+    score_tb Integer,
     match_id varchar(100) NOT NULL PRIMARY KEY
 );
