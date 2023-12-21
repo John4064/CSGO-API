bf95b520b4209175de6595d47a0a8a51333809c4
John Parkhurst <jparkhurst120@gmail.com>
2023-12-21 01:05:47 -0500

    Config Update

--- src/main/java/tech/parkhurst/restapi/config/MongoConfig.java
+++ src/main/java/tech/parkhurst/restapi/config/MongoConfig.java
@@ -4,22 +4,20 @@ import com.mongodb.ConnectionString;
 import com.mongodb.MongoClientSettings;
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.config.AbstractMongoClientConfiguration;
+import org.springframework.data.mongodb.core.MongoTemplate;
 
 import java.util.Collection;
 import java.util.Collections;
 
 @Configuration
-public class MongoConfig extends AbstractMongoClientConfiguration {
+public class MongoConfig{
 
-    @Override
-    protected String getDatabaseName() {
-        return "csgoapi";
-    }
-    @Override
-    public MongoClient mongoClient() {
-        ConnectionString connectionString = new ConnectionString("mongodb://localhost:27017/test");
+    @Bean
+    public MongoClient mongo() {
+        ConnectionString connectionString = new ConnectionString("mongodb://localhost:27017");
         MongoClientSettings mongoClientSettings = MongoClientSettings.builder()
                 .applyConnectionString(connectionString)
                 .build();
@@ -27,8 +25,8 @@ public class MongoConfig extends AbstractMongoClientConfiguration {
         return MongoClients.create(mongoClientSettings);
     }
 
-    @Override
-    public Collection<String> getMappingBasePackages() {
-        return Collections.singleton("tech.parkhurst.restapi");
+    @Bean
+    public MongoTemplate mongoTemplate() throws Exception {
+        return new MongoTemplate(mongo(), "csgoapi");
     }
 }
