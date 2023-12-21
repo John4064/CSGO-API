f1cafe3ca08dbb7353c96df087026081f35087eb
John Parkhurst <jparkhurst120@gmail.com>
2023-12-21 01:04:30 -0500

    Mongodb Data Setup

---
+++ src/main/java/tech/parkhurst/restapi/config/MongoConfig.java
@@ -0,0 +1,34 @@
+package tech.parkhurst.restapi.config;
+
+import com.mongodb.ConnectionString;
+import com.mongodb.MongoClientSettings;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.config.AbstractMongoClientConfiguration;
+
+import java.util.Collection;
+import java.util.Collections;
+
+@Configuration
+public class MongoConfig extends AbstractMongoClientConfiguration {
+
+    @Override
+    protected String getDatabaseName() {
+        return "csgoapi";
+    }
+    @Override
+    public MongoClient mongoClient() {
+        ConnectionString connectionString = new ConnectionString("mongodb://localhost:27017/test");
+        MongoClientSettings mongoClientSettings = MongoClientSettings.builder()
+                .applyConnectionString(connectionString)
+                .build();
+
+        return MongoClients.create(mongoClientSettings);
+    }
+
+    @Override
+    public Collection<String> getMappingBasePackages() {
+        return Collections.singleton("tech.parkhurst.restapi");
+    }
+}
